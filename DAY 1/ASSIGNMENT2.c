ce3ec5273744405ab53c14a28849bf11b2ca2974
ModhurimaGanguly <modhurimaganguly.8i.47@gmail.com>
2025-08-11 23:44:44 +0530

    Add files via upload

---
+++ DAY 1/ASSIGNMENT2.c
@@ -0,0 +1,8 @@
+//Assignment 2: Write a program in C to add two float numbers.
+#include<stdio.h>
+int main()
+{
+	float f1=5,f2=10;
+	printf("Sum: %f",f1+f2);
+	return 0;
+}
---
+++ DAY 1/ASSIGNMENT4.c
@@ -0,0 +1,18 @@
+//Assignment 4: Write a program to print multiplication table of the number given by user without using loop.
+#include<stdio.h>
+int main()
+{
+	float f;
+	printf("Enter number: ");
+	scanf("%f",&f);
+	printf("%f*1=%f\n",f,f*1);
+	printf("%f*2=%f\n",f,f*2);
+	printf("%f*3=%f\n",f,f*3);
+	printf("%f*4=%f\n",f,f*4);
+	printf("%f*5=%f\n",f,f*5);
+	printf("%f*6=%f\n",f,f*6);
+	printf("%f*7=%f\n",f,f*7);	
+	printf("%f*8=%f\n",f,f*8);
+	printf("%f*9=%f\n",f,f*9);
+	printf("%f*10=%f\n",f,f*10);
+}
