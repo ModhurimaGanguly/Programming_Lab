d33539c861fdf4e72bf83b217f5fc844ee7f84e9
ModhurimaGanguly <modhurimaganguly.8i.47@gmail.com>
2025-08-11 23:55:39 +0530

    Delete DAY 1 directory

--- DAY 1/ASSIGNMENT1.c
+++
@@ -1,7 +0,0 @@
-//Assignment 1: Write a program in C to print "Adamas University."
-#include<stdio.h>
-int main()
-{
-	printf("Adamas University");
-	return 0;
-}
--- DAY 1/ASSIGNMENT2.c
+++
@@ -1,8 +0,0 @@
-//Assignment 2: Write a program in C to add two float numbers.
-#include<stdio.h>
-int main()
-{
-	float f1=5,f2=10;
-	printf("Sum: %f",f1+f2);
-	return 0;
-}
